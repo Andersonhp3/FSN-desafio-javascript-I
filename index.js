bac3781e6e7681ac13aa37553d648bf69034e9c0
Andersonhp3 <arhp1591@gmail.com>
2020-03-10 18:01:57 -0300

    Adicionando  a funcionalidade Listar Alunos

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
 // Base a ser utilizada
-const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
+const alunosDaEscola = [{
+    nome: "Henrique",
+    notas: [],
+    cursos: [],
+    faltas: 5
+}, {
+    nome: "Edson",
+    notas: [],
+    cursos: [],
+    faltas: 2
+}, {
+    nome: "Bruno",
+    notas: [10, 9.8, 9.6],
+    cursos: [],
+    faltas: 0
+}, {
+    nome: "Guilherme",
+    notas: [10, 9.8, 9.6],
+    cursos: [{
+        nomeDoCurso: "Full Stack",
+        dataMatricula: new Date
+    }],
+    faltas: 0
+}, {
+    nome: "Carlos",
+    notas: [],
+    cursos: [],
+    faltas: 0
+}, {
+    nome: "Lucca",
+    notas: [10, 9.8, 9.6],
+    cursos: [{
+        nomeDoCurso: "UX",
+        dataMatricula: new Date
+    }],
+    faltas: 0
+}];
+
+const adicionarAluno = (nome) => {
+
+    alunosDaEscola.push({
+        nome: nome
+    });
+}
 
 
-// implementação
+const listarAlunos = (alunos) => {
+    console.log("---Alunos da escola---");
+    for (let aluno in alunos) {
+        console.log(`------------------
+        Nome: ${alunos[aluno].nome}
+        Faltas: ${alunos[aluno].faltas}
+        Notas: ${alunos[aluno].notas}`);
+        for (let curso in alunos[aluno].cursos) {
+            console.log(`
+        Curso: ${alunos[aluno].cursos[curso].nomeDoCurso}
+        Matricula: ${alunos[aluno].cursos[curso].dataMatricula}`);
+        }
+    }
+
+
+    /*Com essa função o usuário poderá ver todos os alunos cadastrados atualmente no sistema. 
+    Vale dizer que As informações deverão ser exibidas em um formato amigável.*/
+}
+
+
+listarAlunos(alunosDaEscola);
